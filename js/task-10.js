27112519bb2fbbefad8b394b7b67811e33654cf9
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-07-03 18:14:09 +0300

    10th task started

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputReff = document.querySelector("input");
+const createButtonReff = document.querySelector("[data-create]");
+const destriyButtonReff = document.querySelector("[data-destroy]");
+let inputValue = 0;
+
+inputReff.addEventListener("change", onInputChange);
+createButtonReff.addEventListener("click", onCreateButtonClick);
+destriyButtonReff.addEventListener("click", onDestroyButtonClick);
+
+function onInputChange(event) {
+  inputValue = Number(event.currentTarget.value);
+}
+
+function onCreateButtonClick() {
+  function createBoxes(amount) {
+    console.log(amount);
+  }
+
+  return createBoxes(inputValue);
+}
+
+function onDestroyButtonClick() {
+  console.log(inputValue);
+}
