ff5bd26082a71aa96d8ca9bcb740b1a7e911bb69
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-07-04 13:33:18 +0300

    finish ho,e work

--- js/task-10.js
+++ js/task-10.js
@@ -5,6 +5,7 @@ function getRandomHexColor() {
 const inputReff = document.querySelector("input");
 const createButtonReff = document.querySelector("[data-create]");
 const destriyButtonReff = document.querySelector("[data-destroy]");
+const boxesDivReff = document.querySelector("#boxes");
 let inputValue = 0;
 
 inputReff.addEventListener("change", onInputChange);
@@ -17,12 +18,22 @@ function onInputChange(event) {
 
 function onCreateButtonClick() {
   function createBoxes(amount) {
-    console.log(amount);
-  }
+    const BoxesArray = [];
+    let widthValue = 30;
+    for (let i = 1; i <= amount; i += 1) {
+      const boxMarkupElement = document.createElement("div");
+      boxMarkupElement.style.width = `${widthValue}px`;
+      boxMarkupElement.style.height = `${widthValue}px`;
+      boxMarkupElement.style.backgroundColor = `${getRandomHexColor()}`;
+      BoxesArray.push(boxMarkupElement);
+      widthValue += 10;
+    }
 
+    boxesDivReff.append(...BoxesArray);
+  }
   return createBoxes(inputValue);
 }
 
 function onDestroyButtonClick() {
-  console.log(inputValue);
+  boxesDivReff.innerHTML = "";
 }
