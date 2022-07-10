623ab89d821a2438a08e54ec2062b05aaba78351
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-07-10 13:53:32 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,6 +5,7 @@ inputFieldReff.addEventListener("blur", onInputFieldBlur);
 function onInputFieldBlur({ currentTarget }) {
   const requiredLength = Number(currentTarget.getAttribute("data-length"));
   if (currentTarget.value.length !== requiredLength) {
+    currentTarget.classList.remove("valid");
     currentTarget.classList.add("invalid");
   } else {
     currentTarget.classList.remove("invalid");
